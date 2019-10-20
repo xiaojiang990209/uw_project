1ccaadbdc3f6b1847a30e49843ff2e8367ae6271
Roger Jiang <xiaojiang990209@gmail.com>
2019-10-19 21:53:01 -0400

    Task/Refactor course components (#4)
    * Refactor course components
    
    * fix comments

---
+++ client/src/containers/Course/Course.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Select from 'react-select';
+import { Container, Row, Col, Button } from 'reactstrap';
+import CourseDetail from './CourseDetail';
+import { courseCodes, semesters } from '../../utils/courseCodes';
+import { getCourseSchedule } from '../../actions/courseActions'
+import { Wrapper } from './components';
+
+const subjects = courseCodes.map(x => ({value: x, label: x}));
+const terms = semesters.map(x => ({value: x, label: x}));
+
+const styles = {
+  control: styles => ({ ...styles, backgroundColor: 'white', margin: '1em auto' }),
+}
+
+function Course(props) {
+  const [selectedSubject, setSelectedSubject] = useState(null);
+  const [selectedTerm, setSelectedTerm] = useState(null);
+  const [showCourseComponent, setShowCourseComponent] = useState(false);
+  const [courses, setCourses] = useState([]);
+
+  const submitCourse = e => {
+      e.preventDefault();
+      props.getCourseSchedule(selectedTerm.value, selectedSubject.value)
+          .then(res => { setCourses(res.data); setShowCourseComponent(true); })
+          .catch(err => console.log(err));
+  }
+
+  const generateCourseDetails = courses => courses.map((value, index) => (
+    <CourseDetail key={index} course={value} />
+  ));
+
+  return (
+    <Wrapper>
+      <Container>
+        <Row>
+          <Col md={{ size: 4, offset: 4}}>
+            <Select
+              value={selectedSubject}
+                onChange={setSelectedSubject}
+                options={subjects}
+                styles={styles}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={{ size: 4, offset: 4}}>
+            <Select
+              value={selectedTerm}
+              onChange={setSelectedTerm}
+              options={terms}
+              styles={styles}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={{ size: 4, offset: 5}}>
+            <Button outline color='primary' onClick={submitCourse}>Get Courses</Button>
+          </Col>
+        </Row>
+      </Container>
+      {showCourseComponent && generateCourseDetails(courses)}
+    </Wrapper>
+  );
+}
+
+Course.propTypes = {
+  getCourseSchedule: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+export default connect(
+  mapStateToProps,
+  { getCourseSchedule }
+)(Course);
---
+++ client/src/containers/Course/CourseDetail.js
@@ -0,0 +1,129 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Row, Col, Button, Table, Collapse, Card, CardBody, CardText } from 'reactstrap';
+import { getProfRating, getCourseDescription } from '../../actions/courseActions';
+import { TextWrapper, BoldTitle } from './components';
+
+function CourseDetail(props) {
+  const [showDetail, setShowDetail] = useState(false);
+
+  useEffect(() => setShowDetail(false), [props.course]);
+
+  const fetchCourseDescription = (course) => {
+    let distinctInstructors = [...new Set(course.sections.filter(x => x.instructor).map(x => x.instructor))];
+    distinctInstructors.forEach(x => {
+      if (!(x in props.ratingsMap)) {
+        props.getProfRating(x);
+      }
+    })
+    if (!(course.name in props.courseDescriptions)) {
+      props.getCourseDescription(course.name);
+    }
+  }
+
+  const renderCourseTitle = (course) => (<span>{course.name}: {course.title}</span>);
+
+  const renderProfRating = (instructor) => {
+    let ratingRow = <td>{instructor}</td>
+    let rating = props.ratingsMap[instructor];
+    if (rating && rating.score) {
+        ratingRow = (
+            <td><a href={rating.url}>{instructor} ({rating.score})</a></td>
+        );
+    } 
+    return ratingRow;
+}
+
+  const renderCourseInfo = (course) => {
+    let infoSection;
+    let info = props.courseDescriptions[course.name];
+    if (info !== undefined && info !== null) {
+        infoSection = (
+            <TextWrapper>
+              <BoldTitle>Description</BoldTitle>
+              <CardText>{info.description}</CardText>
+              <BoldTitle>Prerequisites</BoldTitle>
+              <CardText>{info.prerequisites || "None"}</CardText>
+              <BoldTitle>Antirequisites</BoldTitle>
+              <CardText>{info.antirequisites || "None"}</CardText>
+            </TextWrapper>
+        );
+    }
+    return infoSection;
+  }
+
+  const renderCourseSections = (course) => {
+    const sections = course.sections.map((value,index) => {
+      const timeSlot = value.start === null ? <td>TBA</td> : <td>{value.start}-{value.end}, {value.days}</td>
+      return (
+          <tr key={index}>
+              <td>{value.section}</td>
+              <td>{value.class_number}</td>
+              <td>{value.total} / {value.capacity}</td>
+              {timeSlot}
+              <td>{value.location}</td>
+              {renderProfRating(value.instructor)}
+          </tr>
+      )
+    });
+    return (
+      <TextWrapper>
+        <Table size="sm">
+          <thead>
+            <tr>
+              <th>Section</th>
+              <th>Class</th>
+              <th>Enrolled</th>
+              <th>Time</th>
+              <th>Location</th>
+              <th>Instructor</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sections}
+          </tbody>
+        </Table>
+      </TextWrapper>
+    )
+  }
+
+  const onDetailClicked = (e) => {
+    e.preventDefault();
+    if (!showDetail) { fetchCourseDescription(props.course); }
+    setShowDetail(!showDetail);
+  }
+
+  return (
+    <Row style={{marginTop: '5px'}}>
+      <Col md={{ size: 8, offset: 2}}>
+        <Button color="primary" size="md" onClick={onDetailClicked} block>{renderCourseTitle(props.course)}</Button>
+        <Collapse isOpen={showDetail}>
+          <Card outline color="primary">
+            <CardBody>
+              {renderCourseInfo(props.course)}<br/>
+              {renderCourseSections(props.course)}
+            </CardBody>
+          </Card>
+        </Collapse>
+      </Col>
+    </Row>
+  )
+}
+
+CourseDetail.propTypes = {
+  getProfRating: PropTypes.func.isRequired,
+  getCourseDescription: PropTypes.func.isRequired,
+  ratingsMap: PropTypes.object.isRequired,
+  courseDescriptions: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  ratingsMap: state.course.ratingsMap,
+  courseDescriptions: state.course.descriptions,
+});
+
+export default connect(
+  mapStateToProps,
+  { getProfRating, getCourseDescription }
+)(CourseDetail);
