a8d74756288f6570a68c10dfb8c664bcb50c1ca1
Zijie Jiang <z224jiang@edu.uwaterloo.ca>
2019-10-20 23:31:30 -0400

    fix comments

--- client/src/containers/Navbar/Navbar.js
+++ client/src/containers/Navbar/Navbar.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { slide as Menu } from 'react-burger-menu';
 import "./Navbar.css";
+
+const Navbar = (props) =>
+  <Menu disableAutoFocus width={ 256 }>
+    {props.options.map(({ route, name }) => (
+      <Link to={route}>{name}</Link>
+    ))}
+  </Menu>
   
-export default (props) => (
-    <Menu disableAutoFocus width={ 256 }>
-      {props.options.map(({ route, name }) => (
-        <Link to={route}>{name}</Link>
-      ))}
-    </Menu>
-)
+export default Navbar;
