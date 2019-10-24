982669194bc99f16e5e8396cf8c4f3e2875eddde
Roger Jiang <rogerjiang@ritual.co>
2019-10-24 12:31:13 -0400

    Add weather setup #1

---
+++ client/src/components/Weather/utils/OpenWeatherApi.js
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import utils from './utils';
+
+export default class OpenWeatherApi {
+  constructor(unit, apiKey, lang) {
+    this.unit = unit;
+    this.apiKey = apiKey;
+    this.baseApiUrl = '//api.openweathermap.org/data/2.5';
+    this.lang = lang;
+  }
+  getForecast(args) {
+    console.log("fetching is invoked");
+    const endPointToday = `${this.baseApiUrl}/weather`;
+    const params = Object.assign(
+      {
+        appid: this.apiKey,
+        lang: this.lang,
+        units: this.unit,
+      },
+      args
+    );
+
+    return axios.get(endPointToday, { params })
+      .then(todayResponse => {
+        const todayData = todayResponse.data;
+        if (todayData) return this._map(todayData, params.lang);
+        return {};
+      });
+  }
+
+  _map(today, lang) {
+    const mapped = {};
+    mapped.location = today.name;
+    mapped.days = [
+      {
+        date: utils.formatDate(today.dt, lang),
+        description: today.weather[0] ? today.weather[0].main : null,
+        icon: today.weather[0] ? today.weather[0].icon : null,
+        temperature: {
+          min: today.main.temp_min.toFixed(0),
+          max: today.main.temp_max.toFixed(0),
+        },
+        wind: today.wind.speed.toFixed(0),
+        humidity: today.main.humidity,
+      }
+    ]
+    return mapped;
+  }
+}
--- client/src/containers/Dashboard/dashboard.js
+++ client/src/containers/Dashboard/dashboard.js
@@ -1,47 +1,53 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { logoutUser } from '../../ducks/session';
+import Weather from '../../components/Weather';
 
-class Dashboard extends Component {
-  onLogoutClick = (e) => {
-    e.preventDefault();
-    this.props.logoutUser();
-  };
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.session.isAuthenticated) {
-      this.props.history.push('/login');
-    }
-  }
-
-  render() {
-    return (
-      <div style={{ height: '75vh' }} className="container valign-wrapper">
-        <div className="row">
-          <div className="col s12 center-align">
-            <h4>
-              <p className="flow-text grey-text text-darken-1">
-                You're logged into a fullstack MERN app
-              </p>
-            </h4>
-            <button
-              style={{
-                width: '150px',
-                borderRadius: '3px',
-                letterSpacing: '1.5px',
-                marginTop: '1rem',
-              }}
-              onClick={this.onLogoutClick}
-              className="btn btn-large wave-effects wave-light hoverable blue accent-3"
-            >
-              Log out
-            </button>
-          </div>
-        </div>
-      </div>
-    );
-  }
+function Dashboard(props) {
+  return <Weather />;
 }
+
+// class Dashboard extends Component {
+//   onLogoutClick = (e) => {
+//     e.preventDefault();
+//     this.props.logoutUser();
+//   };
+
+//   componentWillReceiveProps(nextProps) {
+//     if (nextProps.session.isAuthenticated) {
+//       this.props.history.push('/login');
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div style={{ height: '75vh' }} className="container valign-wrapper">
+//         <div className="row">
+//           <div className="col s12 center-align">
+//             <h4>
+//               <p className="flow-text grey-text text-darken-1">
+//                 You're logged into a fullstack MERN app
+//               </p>
+//             </h4>
+//             <button
+//               style={{
+//                 width: '150px',
+//                 borderRadius: '3px',
+//                 letterSpacing: '1.5px',
+//                 marginTop: '1rem',
+//               }}
+//               onClick={this.onLogoutClick}
+//               className="btn btn-large wave-effects wave-light hoverable blue accent-3"
+//             >
+//               Log out
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 const mapStateToProps = (state) => ({
   session: state.session,
