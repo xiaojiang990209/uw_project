d49cd8471024dd26336b2ac1fb4ac7238939d2ff
Zijie Jiang <z224jiang@edu.uwaterloo.ca>
2019-10-27 12:16:06 -0400

    task/fix proxy server

--- client/src/client/session.js
+++ client/src/client/session.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 const SESSION_PATH = 'users';
 
 const sessionClient = {
-  register: (user) => axios.post(`/${SESSION_PATH}/register`, user),
-  login: (user) => axios.post(`${SESSION_PATH}/login`, user),
+  register: (user) => axios.post(`/api/${SESSION_PATH}/register`, user),
+  login: (user) => axios.post(`/api/${SESSION_PATH}/login`, user),
 };
 
 export default sessionClient;
