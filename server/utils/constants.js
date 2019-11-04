ecb8bf2ec967a743dc6d051a8b8d096b3213ea76
Roger Jiang <xiaojiang990209@gmail.com>
2019-11-04 18:34:43 -0500

    task/refactor uw api requests (#13)
    * refactor uw api requests; add important dates and info session
    
    * data is json array; needs to be iterated

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -3,5 +3,6 @@ module.exports = {
     RMP_PROF_URL: 'https://www.ratemyprofessors.com/ShowRatings.jsp?tid=',
     RATING_KEY: 'averageratingscore_rf',
     ID_KEY: 'pk_id',
-    LAST_UPDATED: 'last_updated'
+    LAST_UPDATED: 'last_updated',
+    SEMESTERS: ['1195', '1199', '1201']
 };
