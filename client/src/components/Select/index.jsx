1c3931cb7b2bbe66a35dfef7cb332eaaef7a1342
Zijie Jiang <z224jiang@edu.uwaterloo.ca>
2019-11-24 14:12:47 -0500

    Add required react-select

---
+++ client/src/components/Select/index.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import BaseSelect from 'react-select';
+
+const Select = (props) => {
+  const [selectedRef, setSelectedRef] = useState(null);
+  const [value, setValue] = useState("");
+  const { required, ...rest } = props;
+
+  const noop = () => {}
+
+  const onChange = (value, meta) => {
+    props.onChange(value, meta);
+    setValue(value);
+  }
+
+  return (
+    <div>
+      <BaseSelect {...rest} ref={setSelectedRef} onChange={onChange}/>
+      <input
+            tabIndex={-1}
+            autoComplete="off"
+            style={{
+              opacity: 0,
+              width: "100%",
+              height: 0,
+              position: "absolute"
+            }}
+            value={value}
+            onChange={noop}
+            onFocus={() => selectedRef.focus()}
+            required={required}
+          />
+    </div>
+  );
+}
+
+export default Select;
