fb384c5f15b1f6e2ec38da7202ebf30c75d4b956
Zijie Jiang <z224jiang@edu.uwaterloo.ca>
2019-12-14 17:55:47 -0500

    Some stupid media query for weather

---
+++ client/src/components/Weather/component/index.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const TodayForecastContainer = styled.div`
+  font-size: 13px;
+  font-weight: bold;
+  box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+`;
+
