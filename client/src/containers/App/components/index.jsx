a9e121e6944c4ef55a51416cc0ff5b275fd9853b
Roger Jiang <xiaojiang990209@gmail.com>
2019-12-15 18:41:14 -0500

    Fix media query (#32)

--- client/src/containers/App/components/index.jsx
+++ client/src/containers/App/components/index.jsx
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 
 export const MainContainer = styled.div`
-  width: 75%;
+  width: 80%;
   margin: ${({theme}) => theme.unit.large}px auto;
+  @media (max-width: 1024px) {
+    width: 90%;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
