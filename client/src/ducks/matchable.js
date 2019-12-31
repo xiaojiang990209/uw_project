3f3a968b0bac8e0644eb4521edfddf104b5351e3
Roger Jiang <xiaojiang990209@gmail.com>
2019-12-30 22:20:42 -0500

    Bugfix/rename userID to userId (#39)

--- client/src/ducks/matchable.js
+++ client/src/ducks/matchable.js
@@ -2,18 +2,18 @@ import axios from 'axios';
 import withNotification from '../utils/sendNotification';
 import { CREATE_GROUP_MESSAGE, MATCH_GROUP_MESSAGE, UPDATE_GROUP_MESSAGE } from './constants';
 
-export const matchGroup = (userID, maxMembers, courseID, date, hasTime) => {
-  const body = { userID, maxMembers, courseID, date, hasTime };
+export const matchGroup = (userId, maxMembers, courseID, date, hasTime) => {
+  const body = { userId, maxMembers, courseID, date, hasTime };
   return withNotification(MATCH_GROUP_MESSAGE)(axios.post('/api/matchable/current-groups', body));
 }
 
-export const joinGroup = (groupID, userID) => {
-  const body = { groupID, userID };
+export const joinGroup = (groupID, userId) => {
+  const body = { groupID, userId };
   return withNotification(UPDATE_GROUP_MESSAGE)(axios.post('/api/matchable/update-group', body));
 }
 
-export const createGroup = (userID, startDate, courseID, groupSize, duration) => {
-  const body = { userID, groupSize, startDate, courseID, duration };
+export const createGroup = (userId, startDate, courseID, groupSize, duration) => {
+  const body = { userId, groupSize, startDate, courseID, duration };
   return withNotification(CREATE_GROUP_MESSAGE)(axios.post('/api/matchable/groups', body).then(res => res.data));
 }
 
