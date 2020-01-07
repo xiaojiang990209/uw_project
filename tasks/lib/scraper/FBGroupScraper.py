ae3f838f3a40bda9652e261c6083a2d1b62e3aaa
Roger Jiang <xiaojiang990209@gmail.com>
2020-01-07 15:05:12 -0500

    Add FB Group scraper (#43)

---
+++ tasks/lib/scraper/FBGroupScraper.py
@@ -0,0 +1,35 @@
+from time import time
+from .BaseScraper import BaseScraper
+
+COLLECTED_AT = 'collected_at';
+
+class FBGroupScraper:
+    _scrape_url = 'https://m.facebook.com/groups/{}'
+    _next_page_url = 'https://m.facebook.com/{}'
+
+    def __init__(self, converter):
+        self.base_scraper = BaseScraper()
+        self.converter = converter
+
+    def get_result(self, group_id=None, num_posts=None):
+        results = []
+        timestamp = int(time())
+        self.converter.set_group_id(group_id)
+        url = self._scrape_url.format(group_id)
+
+        while True:
+            html = self.base_scraper.get_response(url)
+            posts = self.converter.get_posts(html)
+            for post in posts:
+                result = self.converter.convert(post)
+                result[COLLECTED_AT] = timestamp
+                results.append(result)
+                if len(results) == num_posts:
+                    return results
+
+            url = self._next_page_url.format(
+                    self.converter.get_next_page_url(html))
+
+        return results
+
+
