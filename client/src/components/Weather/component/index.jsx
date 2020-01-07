a7f76394652dcab26b55e0a766c328908f3b2b49
winnie <winnie@voiceflow.com>
2020-01-07 00:46:29 -0500

    fix: comment

--- client/src/components/Weather/component/index.jsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const TodayForecastContainer = styled.div`
-  font-size: 13px;
-  font-weight: bold;
-  box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
-`;
-
