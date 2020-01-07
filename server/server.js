f6b489d352624506ff4ab6df95fbacff38edcc7b
Yawen <35966017+yawenwinnie@users.noreply.github.com>
2020-01-07 17:11:41 -0500

    to master (#48)
    * to master
    
    * fixed
    
    Co-authored-by: WinnieWang-Winter2019 <54855650+WinnieWang-Winter2019@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ const  io = require('socket.io')(server);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static('client/build'))
 
 /*
  * Initialize MongoDB database
