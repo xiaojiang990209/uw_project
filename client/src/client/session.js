77707517ffdabdb8d09375d840f8f0b9c76f6838
Zijie Jiang <z224jiang@edu.uwaterloo.ca>
2020-01-17 15:33:39 -0500

    fix favourite course not saved

--- client/src/client/session.js
+++ client/src/client/session.js
@@ -5,8 +5,8 @@ const SESSION_PATH = 'users';
 const sessionClient = {
   register: (user) => axios.post(`/api/${SESSION_PATH}/register`, user),
   login: (user) => axios.post(`/api/${SESSION_PATH}/login`, user),
-  saveFavouriteCourses: (userId, favouriteCourses) =>
-    axios.put(`/api/${SESSION_PATH}/favouriteCourses`, { userId, favouriteCourses }),
+  saveFavouriteCourses: (favouriteCourses) =>
+    axios.put(`/api/${SESSION_PATH}/favouriteCourses`, { favouriteCourses }),
 };
 
 export default sessionClient;
