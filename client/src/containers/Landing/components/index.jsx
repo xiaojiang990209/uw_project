3f67c49b6150c70f9732ea47b183e22a6a71902e
Zijie Jiang <z224jiang@edu.uwaterloo.ca>
2020-01-23 13:20:17 -0500

    redo landing

--- client/src/containers/Landing/components/index.jsx
+++ client/src/containers/Landing/components/index.jsx
@@ -1,104 +1,24 @@
-import React from 'react';
 import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBook, faCar, faHome, faUniversity } from '@fortawesome/free-solid-svg-icons';
-import { Container } from 'reactstrap';
+import { Container, Row } from 'reactstrap';
 import Button from '../../../components/Button';
 
-const LANDING_CONTENT = {
-  course: {
-    icon: faUniversity,
-    title: 'Course Information',
-    content: 'See course offerings, availabilities and professor ratings in one shot!'
-  },
-  housing: {
-    icon: faHome,
-    title: 'Housing',
-    content: 'Have you figured out where you will be living next term?'
-  },
-  carpool: {
-    icon: faCar,
-    title: 'Carpool',
-    content: 'Find available carpools in Waterloo, Toronto and many other cities!'
-  },
-  matchable: {
-    icon: faBook,
-    title: 'Study Buddies',
-    content: 'Create course-based study groups and tackle difficult problems together!'
-  },
-}
-
-const StyledIconWrapper = styled.div`
-  color: #007bff;
-  width: 100%;
-  text-align: center;
-  margin: ${({theme}) => theme.unit.medium}px auto;
-`;
-
-const StyledLandingTitle = styled.p`
-  font-size: 21px;
-  text-align: center;
-  -webkit-font-smoothing: auto;
-`;
-
-const StyledLandingContent = styled.p`
-  font-size: 15px;
-  text-align: center;
-  -webkit-font-smoothing: auto;
-`;
-
-const LandingCard = (props) => {
-  const { icon, title, content, onClick } = props;
-  return (
-    <div onClick={onClick}>
-      <StyledIconWrapper><FontAwesomeIcon icon={icon} size="5x"/></StyledIconWrapper>
-      <StyledLandingTitle>{title}</StyledLandingTitle>
-      <StyledLandingContent>{content}</StyledLandingContent>
-    </div>
-  );
-}
-
-const createLandingCard = (contentProps) => (props) => (
-  <LandingCard onClick={props.onClick} {...contentProps} />
-);
-
-export const CourseLandingCard = createLandingCard(LANDING_CONTENT.course);
-export const HousingLandingCard = createLandingCard(LANDING_CONTENT.housing);
-export const CarpoolLandingCard = createLandingCard(LANDING_CONTENT.carpool);
-export const MatchableLandingCard = createLandingCard(LANDING_CONTENT.matchable);
-
 export const LandingButton = styled(Button)`
-  margin: ${({theme}) => theme.unit.medium}px;
   min-width: 200px;
-  &:hover {
-    background: #e9e9e9;
-  }
-`;
-
-const FooterWrapper = styled.div`
-  position: relative;
-  width: 100vw;
-  left: calc(-50vw + 50%);
-  margin-top: ${({theme}) => 3*theme.unit.medium}px;
+  background: #d7ecff;
+  border: none;
   margin-bottom: ${({theme}) => theme.unit.medium}px;
-  font-weight: bold;
-`;
-
-const HeaderWrapper = styled(FooterWrapper)`
-  margin: ${({theme}) => theme.unit.medium}px auto;
-  color: #fff;
-  background: #343a40;
-  padding: 3rem 0;
-  @media (min-width: 768px) {
-    padding: 10rem 0;
+  &:hover, &:focus, &:active {
+    background: #bde0ff !important;
+    box-shadow: 0px 4px 4px rgba(0,0,0,0.2) !important;
+    color: #000 !important;
   }
 `;
 
 export const StyledTitle = styled.p`
-  font-size: 2.2rem;
-  margin-bottom: 3rem;
+  font-size: 3rem;
+  font-weight: bold;
   @media (max-width: 768px) {
-    font-size: 1.6rem;
+    font-size: 1.8rem;
   }
 `;
 
@@ -114,26 +34,60 @@ export const StyledLogo = styled(StyledFooterText)`
   color: #000;
 `;
 
-export const StyledLink = styled.a`
-  &:hover {
-    text-decoration: none;
+export const LandingBackground = styled.div`
+  position: absolute;
+  z-index: -1;
+  width: 100vw;
+  left: calc(-50vw + 50%);
+  top: 0px;
+  padding-top: ${({theme}) => 2*theme.unit.large}px;
+  padding-bottom: ${({theme}) => theme.unit.large}px;
+  font-weight: bold;
+  background: aliceblue;
+  @media (max-width: 768px) {
+    height: 880px;
+  }
+  @media (min-width: 768px) {
+    height: 820px;
+  }
+  @media (min-width: 1000px) {
+    height: 740px;
+  }
+  @media (min-width: 1200px) {
+    height: 600px;
   }
 `;
 
-export const Content = (props) => (
-  <Container>
-    {props.children}
-  </Container>
-);
+export const Subheading = styled.div`
+  text-align: center;
+  margin-top: 64px;
+  font-weight: bold;
+  font-size: 1.9rem;
+`;
 
-export const Header = (props) => (
-  <HeaderWrapper>
-    <Content {...props} />
-  </HeaderWrapper>
-);
+export const DescriptionContainer = styled.div`
+  text-align: center;
+  font-size: 1.2rem;
+  margin: 24px 0px;
+`
 
-export const Footer = (props) => (
-  <FooterWrapper>
-    <Content {...props} />
-  </FooterWrapper>
-);
+export const ContentRow = styled(Row)`
+  margin: ${({theme}) => theme.unit.large}px 0px;
+`;
+
+export const AppImage = styled.img`
+  width: 500px;
+  border-radius: 16px;
+`;
+
+export const Header = styled(Container)`
+  margin-top: ${({theme}) => theme.unit.large}px;
+`;
+
+export const Content = styled(Container)`
+  margin-top: ${({theme}) => theme.unit.large + theme.unit.medium}px;
+`;
+
+export const Footer = styled(Container)`
+  margin: ${({theme}) => theme.unit.medium}px 0px;
+`;
