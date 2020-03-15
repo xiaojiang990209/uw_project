7ae182b9ba479a50eaafba043e06ed6346987f56
Zijie Jiang <z224jiang@edu.uwaterloo.ca>
2020-03-15 02:11:49 -0400

    Refactor library service

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -1,6 +1,4 @@
 module.exports = {
-    UW_LIB_BOOKING_URL: 'https://bookings.lib.uwaterloo.ca/sbs/day.php',
-    UW_LIB_BOOKING_PREFIX: 'https://bookings.lib.uwaterloo.ca/sbs',
     FB_GROUP_TYPES: Object.freeze({
         "HOUSING": 1,
         "CARPOOL": 2,
